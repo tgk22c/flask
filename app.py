32f6b6e9aff3631ac746a1a1283c09c7513c6b7d
tgk22c <tgk22c@naver.com>
2023-10-04 02:04:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,40 @@
-from flask import Flask, render_template, request
-from PIL import Image
 import os
+from flask import Flask, request, send_from_directory
+from PIL import Image
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+app.config['ALLOWED_EXTENSIONS'] = {'jpg'}
 
-@app.route('/', methods=['GET', 'POST'])
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        if 'file' not in request.files:
-            return 'No file part'
         file = request.files['file']
-        if file.filename == '':
-            return 'No selected file'
-        
-        # 임시로 저장할 경로 지정. 실제 서버에서는 보안상의 이유로 안전한 위치를 선택해야 합니다.
-        temp_path = os.path.join('temp', file.filename)
-        
-        # 파일을 임시 경로에 저장
-        file.save(temp_path)
+        if file and allowed_file(file.filename):
+            filename = file.filename
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
 
-        # 이미지 상하 반전 처리
-        flipped_img = flip_image(temp_path)
+            # 이미지 상하 반전 처리
+            img = Image.open(filepath)
+            img_rotated = img.rotate(180)
+            img_rotated.save(filepath)
 
-        # 결과 이미지를 저장할 경로 지정. 여기서는 원본 파일명에 '_flipped'를 추가합니다.
-        result_path = os.path.join('results', os.path.splitext(file.filename)[0] + '_flipped.jpg')
+            return send_from_directory(directory=app.config['UPLOAD_FOLDER'], filename=filename, as_attachment=True)
 
-        # 결과 이미지 저장
-        flipped_img.save(result_path)
+    return '''
+    <!doctype html>
+    <title>Upload a .jpg File</title>
+    <h1>Upload a .jpg File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
-    return render_template('index.html')
-
-def flip_image(image_path):
-    img = Image.open(image_path)
-    flipped_img = img.transpose(Image.FLIP_TOP_BOTTOM)
-    
-    return flipped_img
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+   app.run(debug=True)

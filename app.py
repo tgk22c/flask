87ce4c19d2f099a07f51e1c2bdc73df92fb04de6
tgk22c <tgk22c@naver.com>
2023-10-04 13:04:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,43 @@
-from flask import Flask, request, send_file
+import os
+from flask import Flask, request, send_from_directory
 from PIL import Image
-import io
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+app.config['ALLOWED_EXTENSIONS'] = {'jpg'}
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if 'picture' in request.files:
-        file = request.files['picture']
-        image = Image.open(file.stream).convert("RGB")
-        flipped_image = image.transpose(Image.FLIP_TOP_BOTTOM)
-        
-        byte_arr = io.BytesIO()
-        flipped_image.save(byte_arr, format='JPEG')
-        
-        byte_arr.seek(0)
-        
-        return send_file(
-            byte_arr,
-            mimetype='image/jpeg',
-            as_attachment=True,
-            attachment_filename='flipped_image.jpg'
-       )
-    else:
-       return "No file uploaded", 400
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            # Ensure the upload directory exists
+            os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+            filename = file.filename
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
+
+            # 이미지 상하 반전 처리
+            img = Image.open(filepath)
+            img_rotated = img.rotate(180)
+            img_rotated.save(filepath)
+
+            return send_from_directory(directory=app.config['UPLOAD_FOLDER'], filename=filename, as_attachment=True)
+
+    return '''
+    <!doctype html>
+    <title>Upload a .jpg File</title>
+    <h1>Upload a .jpg File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+if __name__ == "__main__":
+   app.run(debug=True)

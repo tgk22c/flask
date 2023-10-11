cce464c1d6cbf92071dca3943f134191781ebb1e
tgk22c <tgk22c@naver.com>
2023-10-11 15:04:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,21 @@
-import os
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, jsonify
+from keras.models import load_model
 from PIL import Image
+import numpy as np
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploads/'
-app.config['ALLOWED_EXTENSIONS'] = {'jpg'}
+model = load_model('model.h5')
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+@app.route('/predict', methods=['POST'])
+def predict():
+    file = request.files['image']
+    img_raw = Image.open(file.stream)
+    
+    # Preprocess image here (resize to model input size etc.)
+    
+    img_arr = np.array(img_raw).reshape((1,) + img_raw.size + (1,))
+    
+    prediction = model.predict(img_arr)
+    
+    return jsonify({'prediction': prediction.tolist()})
 
-@app.route("/", methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            # Ensure the upload directory exists
-            os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-
-            filename = file.filename
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(filepath)
-
-            # 이미지 상하 반전 처리
-            img = Image.open(filepath)
-            img_rotated = img.rotate(180)
-            img_rotated.save(filepath)
-
-        return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
-
-    return '''
-    <!doctype html>
-    <title>Upload a .jpg File</title>
-    <h1>Upload a .jpg File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
-
-if __name__ == "__main__":
-   app.run(debug=True)

9030e767d58e804c28a10249948b85526bd85272
tgk22c <tgk22c@naver.com>
2023-10-13 02:30:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
 from flask import Flask, request, jsonify
 from tensorflow.keras.models import load_model 
 import numpy as np
+import json
 
 app=Flask(__name__)
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    data = request.form['data']
     
-    data=request.form['data']
+    # parse string into a list of floats
+    input_data = json.loads(data)
     
-    # parse string of floats into a numpy array with shape (1,784)
-    input_data=np.array([float(pixel_str) for pixel_str in data.split(',')]).reshape(1,-1)
+    # convert list of floats into a numpy array with shape (1,784)
+    input_data = np.array(input_data).reshape(1,-1)
 
-   
     model=load_model('model.h5') 
-
    
     prediction=model.predict(input_data)
 
   
     final_prediction=np.argmax(prediction)
 
-    return jsonify({'prediction': final_prediction.tolist()})
+   return jsonify({'prediction': final_prediction.tolist()})
